64b8a7e74eacd338b523f6a916609d1489bb953a
bruk19 <brukteshome21@gmail.com>
2022-10-25 09:42:19 +0300

    Add method to group controller

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,13 +1,33 @@
 class GroupController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    if user_signed_in?
+      @groups = current_user.groups
+    else
+      render 'users/splash'
+    end
   end
 
   def new
+    @group = current_user.groups.new
   end
 
   def create
+    @user = current_user
+    @group = @user.groups.create(group_params)
+    if @group.save
+      flash[:notice] = 'New category created successfully'
+      redirect_to group_index_path
+    else
+      flash.now[:alert] = 'Category creation failed'
+      render action: 'new'
+    end
   end
 
+  private
+
   def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
