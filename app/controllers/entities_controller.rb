081af65f68a628256219f7a64a173feee1ff1a9d
bruk19 <brukteshome21@gmail.com>
2022-10-25 23:15:22 +0300

    Add transaction views and method

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -29,9 +29,30 @@ class EntitiesController < ApplicationController
     end
   end
 
+  def index_transaction
+    @group = Group.find_by(id: params[:group_id])
+  end
+
+  def new_transaction
+    @entity = Activity.new
+    @groups = current_user.groups.all
+  end
+
+  def create_transaction
+    @entity = Entity.new(entity_params)
+    @entity.author_id = current_user.id
+    if @entity.save
+      flash[:success] = 'Successfully created'
+      redirect_to entities_home_path(@entity.groups.first.id)
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new_transaction'
+    end
+  end
+
   private
 
   def entity_params
-    params.require(:entity).permit(:name, :amount)
+    params.require(:entity).permit(:name, :amount, :group_ids)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :group, only: [:index, :new, :create] do
-    resources :entities, only: [:index, :new, :create]
+    resources :entities, only: [:index, :new, :create, :index_transaction, 
+      :new_transaction, :create_transaction]
   end
   root to: 'group#index'
 
