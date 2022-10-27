0b3c73c10a5b6e37fdd61f8a7e60b31db832a7ff
bruk19 <brukteshome21@gmail.com>
2022-10-27 11:18:04 +0300

    Add all required changes according to the reviewer

--- db/migrate/20222025090441_add_references.rb
+++ db/migrate/20222025090441_add_references.rb
@@ -1,7 +1,7 @@
 class AddReferences < ActiveRecord::Migration[7.0]
   def change
-    add_reference :entities, :user, foreign_key: { to_table: :users }, index: true
-    add_reference :groups, :user, foreign_key: { to_table: :users }, index: true
+    add_reference :entities, :author, foreign_key: { to_table: :users }, index: true
+    add_reference :groups, :author, foreign_key: { to_table: :users }, index: true
     add_reference :group_entities, :group, foreign_key: { to_table: :groups }, index: true
     add_reference :group_entities, :entity, foreign_key: { to_table: :entities }, index: true
   end
